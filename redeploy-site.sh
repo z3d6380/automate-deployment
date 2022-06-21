50b7f84144285765fea9fb4e50ed8a2d464955cc
Luis Moraguez <73666574+z3d6380@users.noreply.github.com>
2022-06-21 16:53:26 -0400

    Flask app now runs as a service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,9 +4,6 @@
 # Written By: Luis Moraguez
 # Description: Automates the process of redeploying a Flask site
 
-# Kill all tmux sessions
-pkill -f tmux
-
 # cd into project directory
 cd ~/flask-portfolio-site
 
@@ -19,5 +16,6 @@ source python3-virtualenv/bin/activate
 # pip install --upgrade pip # May be needed in some cases
 pip install -r requirements.txt
 
-# Start Detached tmux session that enters project directory, enters python virtual environment, starts the Flask server
-tmux new -d -s flask 'cd ~/flask-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0; exec $SHELL'
+# Restart myportfolio service and display status
+systemctl restart myportfolio
+systemctl status myportfolio
