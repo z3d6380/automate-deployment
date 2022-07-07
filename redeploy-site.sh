35659daebdd35804ed13aa7fd6bc6aac4d676f64
Luis Moraguez <73666574+z3d6380@users.noreply.github.com>
2022-07-07 17:41:16 -0400

    Update redeploy-site.sh
    Updated for containerized site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -10,12 +10,6 @@ cd ~/flask-portfolio-site
 # Make sure local git repo has the latest changes from main branch on GitHub
 git fetch --all && git pull origin main
 
-# Enter python virtual environment and install python dependencies using requirements.txt
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-# pip install --upgrade pip # May be needed in some cases
-pip install -r requirements.txt
-
-# Restart myportfolio service and display status
-systemctl restart myportfolio
-systemctl status myportfolio
+# Restart docker containers
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
