ed4e8c1dadd487e35fc4ae56092a0bd0d1c9f113
Luis Moraguez <73666574+z3d6380@users.noreply.github.com>
2022-07-26 12:48:36 -0400

    Add files via upload

---
+++ do_provision_cos8.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# File: do_provision_cos8.sh
+# Written By: Luis Moraguez
+# Description: Provisions a DigitalOcean Droplet that is running Centos Stream 8
+# Usage: ./do_provision_cos8.sh
+
+# Update OS
+dnf update -y
+
+# Install Python
+dnf install -y git python3
+ln -fs $(which python3) /usr/bin/python
+
+# Install Nano text editor
+dnf install -y nano
+
+# Install TMUX
+dnf install -y tmux
+
+# Git Clone Flask App Repo
+git clone https://github.com/z3d6380/flask-portfolio-site
+
+# Enter Repo
+cd ~/flask-portfolio-site
+
+# Install python virtual environment
+python -m venv python3-virtualenv
+
+# Enter python virtual environment
+source python3-virtualenv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install python dependencies
+pip install -r requirements.txt
+
+# Deactivate virtual environment
+deactivate
+
+# Install Docker
+curl -sSL https://get.docker.com/ | sudo sh
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Download redeploy script
+curl -L https://raw.githubusercontent.com/z3d6380/automate-deployment/main/redeploy-site.sh > redeploy-site.sh && chmod 750 ./redeploy-site.sh
+
+# Run redeploy script
+~/redeploy-site.sh
